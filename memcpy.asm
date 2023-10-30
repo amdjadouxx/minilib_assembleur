007b022689256be914b59f8911320108227e9d2d
Amdjadouxx <114904658+amdjadouxx@users.noreply.github.com>
2023-10-30 11:17:50 +0400

    Create memcpy.asm

---
+++ memcpy.asm
@@ -0,0 +1,23 @@
+BITS 64
+
+;copier rsi in rdx, rdi times
+
+section .text
+    global my_memcpy
+
+    my_memcpy:
+        xor rax, rax
+        mov rax, rdi
+        xor rcx, rcx
+        xor r8, r8
+
+    loop_memcpy:
+        cmp rcx, rdx
+        je end_memcpy
+        mov r8b, byte[rsi + rcx]
+        mov byte[rax + rcx], r8b
+        inc rcx
+        jmp loop_memcpy
+    
+    end_memcpy:
+        ret
