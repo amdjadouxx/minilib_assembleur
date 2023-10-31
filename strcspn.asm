cd6a9a5d4df4afe6fc198ba3b79daedae546f758
Amdjadouxx <114904658+amdjadouxx@users.noreply.github.com>
2023-10-31 09:34:10 +0400

    Create strcspn.asm

---
+++ strcspn.asm
@@ -0,0 +1,42 @@
+BITS 64
+
+; return the length before finding one of RSI's characters in RDI
+
+;rdi string ptr
+;rsi string ptr (all chars to found (one has to be found not all))
+
+section .text
+    global my_strcspn
+
+my_strcspn:
+    xor rax, rax
+    xor rcx, rcx
+    xor r8, r8
+    xor r9, r9
+
+loop_str_strcspn:
+    cmp byte[rdi], 0
+    je end_strcspn
+    jmp loop_cmp_strcspn
+
+continue_loop_str_strcspn:
+    inc rdi
+    jmp loop_str_strcspn
+
+loop_cmp_strcspn:
+    cmp byte[rsi + r8], 0
+    je return_to_loop_str_strcspn
+    mov r9b, byte[rsi + r8]
+    cmp r9b, byte[rdi]
+    je end_strcspn
+    inc r8
+    jmp loop_cmp_strcspn
+
+return_to_loop_str_strcspn:
+    inc rcx
+    mov r8, 0
+    jmp continue_loop_str_strcspn
+
+end_strcspn:
+    mov rax, rcx
+    ret
